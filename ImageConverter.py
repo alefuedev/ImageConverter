651f86e67918116cee4f3221ab3bb8570d95ff50
alefuedev <42624248+alefuedev@users.noreply.github.com>
2020-07-21 19:31:07 -0400

    Update ImageConverter.py

--- ImageConverter.py
+++ ImageConverter.py
@@ -8,14 +8,13 @@ file_to_make = sys.argv[2]
 path = os.getcwd()
 path = path + '/' + file_to_open
 
-
 def from_jpg_to_png():
     jpgs = []
     names = []
     for f in files:
         if f.count('.jpg'):
             jpgs.append(f)
-
+            
     for f in jpgs:
         file = Image.open(f'{f}')
         # change save path folder
@@ -24,14 +23,12 @@ def from_jpg_to_png():
         file_name = file.save(f'{f}.png', "png")
         os.rename(f'{f}.png', path + '/' + file_to_make + '/' + f'{f}.png')
 
-
 try:
     os.chdir(path)
 except FileNotFoundError:
     print('please check the name folder to open')
 else:
     files = os.listdir()
-    # check if folder with the name new exist if not created
     if files.count(file_to_make) == 0:
         print(f'Creating folder {file_to_make}')
         os.mkdir(f'{file_to_make}')
